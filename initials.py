2f5174cb372944b298ab123c46f70ddd20411a14
kxy <99234385+Kwasnega@users.noreply.github.com>
2026-05-31 01:22:36 +0000

    Submit Assignment 1- initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials using python
+# Assignment Number: 1
+#
+# Name: Kwarteng Kwame Acheampong
+# STUDENT ID: 2425401602
+# Email: acheampongkwame855@gmail.com
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Kwarteng Kwame Acheampong, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # printing my initials 
+    print()
+    print("...KKA")
+    print()
+    print("...K          K****...K          K****...     A     A****   ")
+    print("...K         KK****...K         KK****...    A A    A****   ")
+    print("...K        K K****...K        K K****...   A   A   A****   ")
+    print("...K       K  K****...K       K  K****...  A     A  A****   ")
+    print("...K      K   K****...K      K   K****... A       A A****   ")
+    print("...KKKKKKKKKKKK****...KKKKKKKKKKKK****...A         AA****   ")
+    print("...K      K   K****...K      K   K****...AAAAAAAAAAAA****   ")
+    print("...K       K  K****...K       K  K****...A          A****   ")
+    print("...K        K K****...K        K K****...A          A****   ")
+    print("...K         KK****...K         KK****...A          A****   ")
+    print()
 
 
 main()
+
